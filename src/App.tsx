57bb00e68bd1d3c4e615f253fc26db8650fca332
akashk2002 <kanyadharaakash@gmail.com>
2024-04-08 15:41:48 +0530

    array3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
 import './App.css';
 
 function App() {
-  const fruits = [{name: 'Mango', id: 1}, {name: 'Apple', id: 2}, {name: 'Jackfruit', id: 3}];
+  // const fruits = [{name: 'Mango', id: 1}, {name: 'Apple', id: 2}, {name: 'Jackfruit', id: 3}];
+
+  const marks = [
+    [10,20,30],
+    [40,50,60],
+    [70,80,90],
+  ]
 
   return (
     <>
-      <h1>Favourite Fruits : </h1>
+      {/* <h1>Favourite Fruits : </h1>
       {
         fruits.map((fruit, index)=> {
 
@@ -15,7 +20,18 @@ function App() {
         
 
         })
+      } */}
+      
+      <h1>Marks of Students</h1>
+      {
+        marks.map((mark) => {
+          return mark.map((item)=>{
+            return <p>{item}</p>
+          })
+        })
       }
+
+      
 
     
     </>
